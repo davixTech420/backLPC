afb087947210167a9abeac71b2f0906070457466
Your Name <you@example.com>
2024-10-24 09:47:38 -0500

    final 90%

--- controllers/authController.js
+++ controllers/authController.js
@@ -11,6 +11,225 @@ const JefeSala = require("../models/JefeSala");
 
 
 
+
+
+
+
+ exports.validarUsuario = async (req,res) => {
+  try{
+    const { nombre, apellido, tipIdentidad, identificacion, telefono, email, password, estado, role } = req.body;
+    
+    // Generar un token con expiración
+    const token = jwt.sign({ nombre,apellido,tipIdentidad,identificacion,telefono,email,password,estado,role }, process.env.JWT_SECRET, { expiresIn: '5m' });
+
+    // Crear enlace de restablecimiento de contraseña
+    const resetLink = `http://localhost:3001/api/auth/registrarConfir/${token}`;
+
+    //autenticacion para enviar los gmails/
+    const oAuth2Client = new google.auth.OAuth2(process.env.GOOGLE_CLIENT_ID, process.env.GOOGLE_CLIENT_SECRET, process.env.GOOGLE_REDIRECT_URI);
+    oAuth2Client.setCredentials({ refresh_token: process.env.GOOGLE_REFRESH_TOKEN });
+
+    const accessToken = await oAuth2Client.getAccessToken();
+
+
+    // Configurar nodemailer con OAuth2
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        type: 'OAuth2',
+        user: 'cristhiandavidamaya93@gmail.com', // El correo electrónico que envía
+        clientId: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        refreshToken: process.env.GOOGLE_REFRESH_TOKEN,
+        accessToken: accessToken.token,
+      },
+    });
+
+    // Enviar el correo al usuario
+    const mailOptions = {
+      from: process.env.EMAIL_USER,
+      to: email,
+      subject: 'Crear Cuenta En Datech LPC',
+      html: `<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <style>
+        body {
+            font-family: 'Arial', sans-serif;
+            background-color: #f4f4f4;
+            color: #333;
+            margin: 0;
+            padding: 0;
+        }
+        .container {
+            width: 100%;
+            max-width: 600px;
+            margin: 0 auto;
+            background-color: #fff;
+            padding: 20px;
+            border-radius: 8px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        }
+        h1 {
+            font-size: 24px;
+            color: #333;
+        }
+        p {
+            font-size: 16px;
+            color: #666;
+            line-height: 1.6;
+        }
+        .button {
+            display: inline-block;
+            padding: 10px 20px;
+            margin-top: 20px;
+            background-color: #007BFF;
+            color: white;
+            text-decoration: none;
+            font-size: 16px;
+            border-radius: 4px;
+            transition: background-color 0.3s ease;
+        }
+        .button:hover {
+            background-color: #0056b3;
+        }
+        .footer {
+            margin-top: 30px;
+            text-align: center;
+            font-size: 12px;
+            color: #999;
+        }
+        .footer p {
+            margin: 0;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <h1>Crear Cuenta</h1>
+        <p>Hola,</p>
+        <p>Recibimos una solicitud para crear una nueva cuenta en DATECH LPC. Haz clic en el botón de abajo para crear la cuenta:</p>
+        <a href="${resetLink}" class="button">Crear Usuario</a>
+        <p>Este enlace expirará en 5 minutos. Si no solicitaste esto, puedes ignorar este correo.</p>
+        <div class="footer">
+            <p>&copy; 2024 Datech. Todos los derechos reservados.</p>
+        </div>
+    </div>
+</body>
+</html>
+`,
+    };
+
+    await transporter.sendMail(mailOptions);
+
+    res.status(200).json({ message: 'Correo Enviado Con Exito' })
+
+  }catch(error){
+res.status(500).json(error);
+  }
+} 
+
+
+
+
+
+exports.registrarConfirmado = async (req, res) => {
+  const token = req.params.token;
+  const decoded = jwt.verify(token, process.env.JWT_SECRET);
+  const { nombre, apellido, tipIdentidad, identificacion, telefono, email, password, estado, role } = decoded;
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = await User.create({
+      nombre,
+      apellido,
+      tipIdentidad,
+      identificacion,
+      telefono,
+      email,
+      password: hashedPassword,
+      estado,
+      role,
+    });
+    switch (role) {
+      case "admin":
+        try {
+
+          const Admin = await Administrador.create({
+            userId: newUser.id,
+          });
+          res.redirect("http://localhost:3000/loginPublic");
+          
+        } catch (error) {
+          res.status(500).json({ message: "Error registrando Admin", error });
+        }
+
+        break;
+      case "cliente":
+        try {
+          const Clien = await Cliente.create({
+            nacionCliente: req.body.nacionCliente,
+            direccion: req.body.direccion,
+            userId: newUser.id,
+          });
+          res.redirect("http://localhost:3000/loginPublic");
+        } catch (error) {
+          res.status(500).json({ message: "Error registrando Cliente", error });
+        }
+
+        break;
+      case "empleado":
+        try {
+          const empleado = await Empleado.create({
+            userId: newUser.id,
+          });
+          res
+            .status(201)
+            .json({
+              message: "Usuario registrado exitosamente",
+              user: newUser,
+              userEmple: empleado,
+            });
+        } catch (error) {
+          res.status(500).json({ message: "Error registrando empleado", error });
+        }
+        break;
+      case "jefesala":
+        try {
+          const jefe = await JefeSala.create({
+            userId: newUser.id,
+            salaId: req.body.salaId
+          });
+          res
+            .status(201)
+            .json({
+              message: "Jefe registrado exitosamente",
+              user: newUser,
+              userJefe: jefe,
+            });
+        } catch (error) {
+          res.status(500).json({ message: "Error registrando Jefe De Sala", error });
+        }
+        break;
+      default:
+        res
+          .status(500)
+          .json({ message: "Error registrando usuario Rol No Valido", error });
+        break;
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Error registrando usuario", error });
+  }
+};
+
+
+
+
+
+
+
 exports.register = async (req, res) => {
   const { nombre, apellido, tipIdentidad, identificacion, telefono, email, password, estado, role } = req.body;
   try {
@@ -146,7 +365,6 @@ exports.login = async (req, res) => {
 /* este codigo es para generar el restbaecimiento de contraseña que se envia al email */
 exports.forgetPassword = async (req, res) => {
   const { email } = req.body;
-
   try {
     const user = await User.findOne({ where: { email } });
     if (!user) {
--- routes/auth.js
+++ routes/auth.js
@@ -5,7 +5,8 @@ const salaController = require("../controllers/salasController");
 const showsController = require("../controllers/showController");
 const jefeSalaController = require("../controllers/jefeSalaController");
 
-router.post('/register', authController.register);
+router.post('/register', authController.validarUsuario);
+router.get('/registrarConfir/:token',authController.registrarConfirmado)
 router.post('/login', authController.login);
 router.get("/teatros",salaController.getSalasConJefe);
 router.get("/shows",showsController.getShowsActivos);
